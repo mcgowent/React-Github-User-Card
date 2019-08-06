4fc77360e1385dc81e00447bc9484d7d9cbd3563
Timothy McGowen <mcgowen17@yahoo.com>
2019-08-06 14:58:11 -0700

    Finished MVP

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import GitCard from './GitCard'
 import Header from './Header'
+import FriendsList from './FriendsList'
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      gitCard: []
+      gitCard: [],
+      gitFriends: []
     }
   }
 
@@ -25,6 +27,14 @@ class App extends React.Component {
       .catch(err => {
         console.log(err)
       })
+    fetch('https://api.github.com/users/mcgowent/followers')
+      .then(response => {
+        return response.json();
+      })
+      .then(gitFriendData => this.setState({ gitFriends: gitFriendData }))
+      .catch(err => {
+        console.log(err)
+      })
   }
 
 
@@ -32,7 +42,12 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
+        {console.log(this.state.gitFriends)}
         <GitCard data={this.state.gitCard} />
+        <h2 className="mid_bar">Github Friends</h2>
+        <div className="flex_wrap">
+          {this.state.gitFriends.map(e => { return (<FriendsList key={e.id} data={e} />) })}
+        </div>
       </div>
     );
   }
